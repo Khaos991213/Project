4a00ac47f4d8d8bed1ff669a70c41ade36161080
Khaos991213 <80815982+Khaos991213@users.noreply.github.com>
2021-03-19 11:26:17 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charser="utf-8"/>
+<title>Ch2_1.php</title>
+</head>
+<body>
+<?php
+$begin = 12;
+$end = 19;
+for ($i = $begin; $i <= $end; $i += 3) { 
+?>
+<p style="font-size:<?php echo $i;?>pt">
+FUCCCCCKKKKKKK
+<?php
+print "</p>";
+}	?>
+</body>
+</html>
+	
+	
+	
